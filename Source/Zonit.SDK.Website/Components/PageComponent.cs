9c18f0f57326df1fc14013b63aad84108e7a48cc
Dawid Haładus <alerinpoland@gmail.com>
2025-02-05 03:27:05 +0100

    Zaktualizowano wersje i dodano nowe funkcje
    Zaktualizowano wersję AssemblyVersion z 0.1.5 do 0.1.6 oraz wersję pakietu Zonit.Extensions.Website z 0.0.34 do 0.0.37 w pliku Directory.Packages.props. Dodano usingi dla Microsoft.AspNetCore.Components oraz Zonit.Extensions.Website w pliku PageComponent.cs. Dodano właściwość BreadcrumbsProvider z atrybutem Inject oraz metodę OnInitialized, która inicjalizuje BreadcrumbsProvider w pliku PageComponent.cs. Zmieniono typografię tekstu z Typo.h6 na Typo.h1 w pliku PageTemplate.razor.

--- Source/Zonit.SDK.Website/Components/PageComponent.cs
+++ Source/Zonit.SDK.Website/Components/PageComponent.cs
@@ -1,6 +1,19 @@
-﻿namespace Zonit.SDK.Website;
+﻿using Microsoft.AspNetCore.Components;
+using Zonit.Extensions.Website;
+
+namespace Zonit.SDK.Website;
 
 public abstract class PageComponent : BaseComponent
 {
+    [Inject]
+    protected IBreadcrumbsProvider BreadcrumbsProvider { get; set; } = default!;
 
+    public List<BreadcrumbsModel>? Breadcrumbs { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        BreadcrumbsProvider.Initialize(Breadcrumbs);
+    }
 }
