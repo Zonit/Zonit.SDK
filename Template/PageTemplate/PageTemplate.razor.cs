2bf6aca09274175283169408092b0131c4534630
Dawid Haładus <alerinpoland@gmail.com>
2025-02-10 06:30:06 +0100

    Dodano atrybuty do klasy w `PageTemplate.razor.cs`
    Dodano atrybuty `[Authorize]` i `[Route("/" + Route)]` do deklaracji klasy `$safeitemname$` w pliku `PageTemplate.razor.cs`.

--- Template/PageTemplate/PageTemplate.razor.cs
+++ Template/PageTemplate/PageTemplate.razor.cs
@@ -7,7 +7,7 @@ namespace $rootnamespace$;
 
 [Authorize]
 [Route("/" + Route)]
-public sealed partial class $safeitemname$ : PageComponent // IAreaWeb, IAreaManager, IAreaManagement, IAreaDiagnostics
+public sealed partial class $safeitemname$ : PageComponent // IAreaWeb, IAreaManager, IAreaManagement, IAreaDiagnostic
 {
     public const string Route = "$safeitemname$";
 
